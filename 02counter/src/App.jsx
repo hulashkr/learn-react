21da619f11fabe37ae06bd95258e6898cae0b9f4
hulashkr <hulashkrcps@gmail.com>
2025-05-16 18:58:49 +0530

    intro to hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const increaseValue = () => {
+    console.log("Value increased ", Math.random())
+    setCounter(counter+1);
+    console.log(counter)
+  }
+  const decreaseValue = () => {
+    console.log("Value decreased ", Math.random())
+    setCounter(counter-1);
+    console.log(counter)
+
+  }
+  return (
+    <>
+    <h1>Counter App</h1>
+    <h2>Current count value: {counter}</h2>
+    <button
+    onClick={increaseValue}
+    >Increase value</button>
+    <br />
+    <button
+    onClick={decreaseValue}
+    >Decrease value</button>
+    <h3>current value: {counter}</h3>
+    </>
+  )
+}
+
+export default App
