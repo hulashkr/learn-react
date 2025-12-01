b35b83ebfdc3a6bd176714420c34306674073906
Hulash Kumar <hulashkrcps@gmail.com>
2025-12-02 01:14:24 +0530

    add buttons to change background

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("olive")
 
   return (
-    <>
-      <h1 class="bg-green-500 text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <div className='w-full h-screen duration-200 ' 
+    style={{backgroundColor: color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap  justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl'>
+          <button className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor: "violet"}}>Violet</button>
+          <button className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor: "indigo"}}>Indigo</button>
+          <button className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor: "blue"}}>Blue</button>
+          <button className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor: "green"}}>Green</button>
+          <button className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor: "yellow"}}>Yellow</button>
+          <button className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor: "orange"}}>Orange</button>
+          <button className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor: "red"}}>Red</button>
+
+          
+
+        </div>
+      </div>
+    </div>
   )
 }
 
